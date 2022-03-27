98feba11a1ad666c2b9fde5098791fcf6fd43291
MarkSmersh <marksmersh01@gmail.com>
2022-03-27 06:43:00 +0300

    code cleaned

--- core/utils/scrapTikTokPage.js
+++ core/utils/scrapTikTokPage.js
@@ -8,7 +8,7 @@ const scrapPage = async (url) => {
     url = url[0]
 
     try {
-        const browser = await puppeteer.launch({})
+        const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
         const page = await browser.newPage()
         await page.goto(url)
         url = page.url().match(expectedTTUrl)[0]
